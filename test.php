769ca89c7cf689d11bc122af41dab023e14fb181
wanniga <41385413+wanniga@users.noreply.github.com>
2018-07-18 23:19:32 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php
+
+$client = new http\Client;
+$request = new http\Client\Request;
+
+$body = new http\Message\Body;
+$body->append(new http\QueryString(array(
+  'datetime' => '2018-07-18T23:00',
+  'message' => 'บอสสโลกเกิดแล้วจ้าาาาา',
+  'type' => 'sticker',
+  'stickerPackageId' => '1',
+  'stickerId' => '5'
+)));
+
+$request->setRequestUrl('https://notify-api.line.me/api/notify');
+$request->setRequestMethod('POST');
+$request->setBody($body);
+
+$request->setHeaders(array(
+  'Postman-Token' => 'c8950332-f769-4427-8499-fe8140b040e8',
+  'Cache-Control' => 'no-cache',
+  'Content-Type' => 'application/x-www-form-urlencoded',
+  'authorization' => 'Bearer X29UjrXRORRYtNK1x6DdXqOEUE1bPXN92K4kjIVKLPZ'
+));
+
+$client->enqueue($request)->send();
+$response = $client->getResponse();
+
+echo $response->getBody();
